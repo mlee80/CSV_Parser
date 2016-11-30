20f50eb0dd99c256070efea1ccd5e2f69cf2d60d
Mary Lee <mlee@stripe.com>
2016-11-29 19:49:48 -0800

    complete

--- csv.rb
+++ csv.rb
@@ -3,28 +3,36 @@ grants = [] # create array
 
 def grant_search(grants, grant_number)
   return if grant_number == nil
-  duplicate_found = false
+  duplicate_index = nil
 
-  grants.each do |grant|
+  grants.each_with_index do |grant, i|
     if grant[:grant_number] == grant_number
-      duplicate_found = true
+      duplicate_index = i
       break
     end
   end
 
-  if duplicate_found
-    p "duplicate"
-  else
-    p "not duplicate"
-  end
+duplicate_index
 
 end
 
-# print each row
+# loop
 CSV.foreach('grants.csv', headers:true) do |row|
   grant_number = row[1]
-grant_search(grants, grant_number)
 
+# remove nil
+  if grant_number == nil
+    next
+  end
+
+index = grant_search(grants, grant_number)
+
+if index
+  vesting = {vest_date: row[2], shares: row[3]}
+  grant_to_update = grants[index]
+  grant_to_update[:vestings] << vesting
+
+  else
 
   grant = {
     account_id: row[0],
@@ -34,9 +42,11 @@ grant_search(grants, grant_number)
     ]
   }
 
-grants << grant
 
+grants << grant
 
 end
 
-## puts grants
+end
+
+puts grants
